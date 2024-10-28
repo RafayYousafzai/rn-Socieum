234b5a376e4d00e2230fe7b5f9f53c7c30272cf0
rafay <rafaykhan.mesh@gmail.com>
2024-10-28 11:13:06 +0500

    New Features

---
+++ app/(tabs)/listBlog.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const listBlog = () => {
+  return (
+    <View>
+      <Text>listBlog</Text>
+    </View>
+  )
+}
+
+export default listBlog
---
+++ app/(tabs)/playerScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const playerScreen = () => {
+  return (
+    <View>
+      <Text>playerScreen</Text>
+    </View>
+  )
+}
+
+export default playerScreen
