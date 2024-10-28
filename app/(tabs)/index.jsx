765217dab0f514bc833606b5db20d7ae5df24152
rafay <rafaykhan.mesh@gmail.com>
2024-10-28 11:31:16 +0500

    New Features

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const home = () => {
+  return (
+    <View>
+      <Text style={{ color: "#fff" }} className="text-4xl text-white">
+        home
+      </Text>
+    </View>
+  );
+};
+
+export default home;
